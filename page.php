b78dcc646f585590f5c928a92621ca68866e5ca3
Joe Murphy <joe.murphy@gmail.com>
2014-04-07 18:00:28 -0600

    Basic markup for the Denver Post blog template

--- page.php
+++ page.php
@@ -2,11 +2,30 @@
 /*
 Template Name: Thermometer Page
 */
+include(get_template_directory() . '/header.php');
+?>
+     <div id="content">
+	  <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+	       <div class="post">
+		    <div id="post-<?php the_ID(); ?>">
+			 <h1><?php the_title(); ?></h1>
+	       
+			 <div class="pagecontent">
+			      <?php the_content(); include_once('template-widget.php'); ?>
+			 </div>
+		    </div>
+	       </div>
+     
+    	       <?php endwhile; else: ?>
+	       <div class="noresults">
+		    <h1>Not Found: 404 Error</h1>
+		    Oops, it appears that page doesn't exist. Well, we're not all perfect, but we try. Can you try this again or maybe visit our <a 
+title="Our Site" href="http://blogs.denverpost.com">Home Page</a> to start fresh. We'll do better next time.
+	       </div>
+	       <?php endif; ?>
+     </div><!-- Closes the content div-->
+     
 
-get_header(); 
-include_once('template-widget.php');
-?>
-sup
-<?php
-get_footer();
-?>
+	<?php include(get_template_directory() . '/sidebar2.php'); ?>
+         
+<?php include(get_template_directory() . '/footer.php'); ?>
