6e65b06578fdd4030eed0588706aa08298ba74f2
freejoe76 <joe.murphy@gmail.com>
2016-09-12 09:25:16 -0600

    Fix deploy

--- deploy.bash
+++ deploy.bash
@@ -3,3 +3,4 @@ php standalone.php
 php template.php
 ./ftp.bash --dir $REMOTE_DIR --host $REMOTE_HOST
 curl -X PURGE http://extras.denverpost.com/app/record-tracker/index.html
+curl -X PURGE http://extras.denverpost.com/app/record-tracker/
